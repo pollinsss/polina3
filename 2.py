835a83f6202c73ef06bd6c7a9ad10ee62f0f81d8
Полина Максимова <gr632_mapev@lab-308-4.tomtit-tomsk.ru>
2026-04-24 11:04:38 +0700

    q

--- 2.py
+++ 2.py
@@ -1,3 +1,15 @@
+favorite_notes = []
+
+def add_favorite_note(note):
+    if not note.strip():
+        return "Заметка не может быть пустой!"
+
+    note_id = len(favorite_notes) + 1
+    favorite_notes.append({
+        'id': note_id,
+        'text': note.strip()
+    })
+    return f"Заметка добавлена в избранное (ID: {note_id})"
 
 
 def show_all_notes():
